db99446638353b28b76ee857dbc2a6566d741fba
Ricardo Ochoa Armenta <ricardo.ochoaarmenta@kuleuven.be>
2025-05-27 12:58:29 +0200

    Adding the Plasma Column problem

--- src/pgen/pgen.hpp
+++ src/pgen/pgen.hpp
@@ -86,13 +86,6 @@ using namespace parthenon::driver::prelude;
 
 void ProblemGenerator(Mesh *pm, parthenon::ParameterInput *pin, MeshData<Real> *md);
 } // namespace kh
-
-namespace lw_implode {
-using namespace parthenon::driver::prelude;
-
-void ProblemGenerator(MeshBlock *pmb, parthenon::ParameterInput *pin);
-} // namespace lw_implode
-
 namespace rand_blast {
 using namespace parthenon::driver::prelude;
 
@@ -127,14 +120,20 @@ using namespace parthenon::driver::prelude;
 
 void ProblemGenerator(Mesh *pm, parthenon::ParameterInput *pin, MeshData<Real> *md);
 void ProblemInitPackageData(ParameterInput *pin, parthenon::StateDescriptor *pkg);
-void ProblemInitTracerData(ParameterInput *pin, parthenon::StateDescriptor *pkg);
 void Driving(MeshData<Real> *md, const parthenon::SimTime &tm, const Real dt);
 void SetPhases(MeshBlock *pmb, ParameterInput *pin);
 void UserWorkBeforeOutput(MeshBlock *pmb, ParameterInput *pin,
                           const parthenon::SimTime &tm);
-TaskStatus ProblemFillTracers(MeshData<Real> *md, const parthenon::SimTime &tm,
-                              const Real dt);
 void Cleanup();
 } // namespace turbulence
+
+namespace PlasmaColumn_fixedboundary {
+using namespace parthenon::driver::prelude;
+
+void ProblemGenerator(MeshBlock *pmb, parthenon::ParameterInput *pin);
+//void ProblemInitPackageData(ParameterInput *pin, parthenon::StateDescriptor *pkg);
+void CustomX3(std::shared_ptr<MeshBlockData<Real>> &mbd, bool coarse);
+
+} // namespace PlasmaColumn_fixedboundary 
 
 #endif // PGEN_PGEN_HPP_
