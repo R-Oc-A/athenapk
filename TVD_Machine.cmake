c6093107643078991e87c0b26d44566cdac4198a
Tom Van Doorsselaere <tom.vandoorsselaere@kuleuven.be>
2025-06-11 10:45:50 +0200

    Updated the installation file to make it work on pikkeling

--- TVD_Machine.cmake
+++ TVD_Machine.cmake
@@ -3,6 +3,9 @@
 
 message(STATUS "Loading machine configuration for Prof. Tom VD machine\n"  "Last tested: 2025-03-12\n\n")
 
+# 1.CUDA Configuration
+set(Kokkos_ENABLE_CUDA ON)
+set(Kokkos_ARCH_ADA89 ON)
 # 2. OpenMPI Configuration
 set(OpenMPI_ROOT_DIR "$OPENMPI_HOME") 
 
@@ -16,18 +19,15 @@ set(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/external/Kokkos/bin/nvcc_wra
 
 # 5. HDF5
 set (PARTHENON_DISABLE_HDF5 OFF )#CACHE BOOL "Enable HDF5")
-set( CMAKE_PREFIX_PATH "$OPENMPI_HOME;$HDF5_ROOT;$CMAKE_PREFIX_PATH")
-set( HDF5_DIR "$HDF5_ROOT")
+set( CMAKE_PREFIX_PATH "$OPENMPI_HOME;$HDF5_HOME;$CMAKE_PREFIX_PATH")
+set( HDF5_DIR "$HDF5_HOME")
 
 
 # common options
 set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Default release build")
 
-if(${MACHINE_VARIANT} MATCHES "cuda")
-# 1.CUDA Configuration
-set(Kokkos_ARCH_ADA89 ON)
-set(Kokkos_ENABLE_CUDA ON)
-endif()
+#if(${MACHINE_VARIANT} MATCHES "cuda")
+#endif()
 
 # Setting launcher options independent of parallel or serial test as the launcher always
 # needs to be called from the batch node (so that the tests are actually run on the
